3ea5cabe81ee6f519cd205791ab1f8b1071870bb
iwpnd <legionaerr@googlemail.com>
2019-05-26 14:54:09 +0200

    v0.2

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='WordSearch',
-      version='0.1',
+      version='0.2',
       description='Search word occurances in input text',
       url='http://github.com/iwpnd/wordsearch',
       author='Benjamin Ramser',
       author_email='ahoi@iwpnd.pw',
       license='MIT',
-      packages=['WordSearch'],
+      packages=[''],
       zip_safe=False)
