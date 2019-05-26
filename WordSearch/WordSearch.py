4c799dd87ea5cd26d46ffbbc6be57bc20c4fffee
iwpnd <legionaerr@googlemail.com>
2019-05-26 15:46:38 +0200

    added additional input validation methods to Search

--- WordSearch/WordSearch.py
+++ WordSearch/WordSearch.py
@@ -18,13 +18,13 @@ class Search:
     tindex -- cursor if exact_match == False to traverse word until boundary
     
     usage:
-                word = 'Bridge',
-                text = 'Bridges be Bridges.'
+                word = "Bridge",
+                text = "Bridges be Bridges."
                 
-                wb = False -> output: [('Bridges', 0, 6), ('Bridges', 11, 17)]
+                wb = False -> output: [("Bridges", 0, 6), ("Bridges", 11, 17)]
                 wb = True  -> output: []
                 
-                wb = False, url = 'http://here.com' -> output: ['http://here.com', ('Bridges', 0, 6), ('Bridges', 11, 17)]
+                wb = False, url = "http://here.com" -> output: ["http://here.com", ("Bridges", 0, 6), ("Bridges", 11, 17)]
                 
     """
 
@@ -41,10 +41,7 @@ class Search:
         self.exact_match = exact_match
         self.case_sensitive = case_sensitive
         self._is_executed = False
-        self.logger.info('search_term: {}'.format(self.search_term))
-        self.logger.info('input_text: {}'.format(self.input_text))
-        self.logger.info('exact_match: {}'.format(self.exact_match))
-        self.logger.info('case_sensitive: {}'.format(self.case_sensitive))
+        
         self._validate_input()
         self._is_sentence()
 
@@ -52,17 +49,12 @@ class Search:
             self.search_term = self.search_term.lower()
             self.input_text = self.input_text.lower()
 
-        if not self._isin_text():
-            self.logger.error('search_term: {} not in input_text: {}'.format(
-                self.search_term,
-                self.input_text
-            ))
-            raise self.NotFound(
-                'search_term: {} not in input_text: {}'.format(
-                    self.search_term,
-                    self.input_text
-                )
-            )
+        self.logger.info("search_term: {}".format(self.search_term))
+        self.logger.info("input_text: {}".format(self.input_text))
+        self.logger.info("exact_match: {}".format(self.exact_match))
+        self.logger.info("case_sensitive: {}".format(self.case_sensitive))
+
+        
 
     def __repr__(self):
         return """WordSearch(search_term="{search_term}", input_text="{input_text}", 
@@ -76,7 +68,7 @@ class Search:
 
     def run(self):
         self.logger.info(
-            'Searching for "{}" in input_text"'.format(self.search_term))
+            "Searching for '{}' in input_text".format(self.search_term))
         if not self._is_executed:
             while self.oindex != -1:
                 self.oindex = self.input_text[self.sindex:].find(
@@ -94,8 +86,8 @@ class Search:
 
                 self.sindex = self.sindex + len(self.search_term) + self.oindex
 
-        self.logger.info('Search completed')
-        self.logger.info('Found: \n {}'.format(self.occurance))
+        self.logger.info("Search completed")
+        self.logger.info("Found: \n {}".format(self.occurance))
         self._is_executed = True
         
 
@@ -106,7 +98,7 @@ class Search:
         if (
             self.oindex != -1 and
             (
-                self.input_text[self.oindex + len(self.search_term) + self.sindex] in ' .' and
+                self.input_text[self.oindex + len(self.search_term) + self.sindex] in " ." and
                 self.oindex + len(self.search_term) +
                 self.sindex < (len(self.input_text))
             )
@@ -130,7 +122,7 @@ class Search:
             self.input_text[
                 self.oindex +
                 len(self.search_term) +
-                self.sindex + self.tindex] != ' '
+                self.sindex + self.tindex] != " "
             and self.oindex + len(self.search_term) +
             self.sindex + self.tindex < (len(self.input_text) - 1)
         ):
@@ -154,8 +146,8 @@ class Search:
     def _is_sentence(self):
         """append sentence delimiter
         """
-        if self.input_text and self.input_text[-1] not in list('!?.'):
-            self.input_text = self.input_text + '.'
+        if self.input_text and self.input_text[-1] not in list("!?."):
+            self.input_text = self.input_text + "."
 
     def _isin_text(self):
         """initial check if search_term is in input_text
@@ -166,34 +158,70 @@ class Search:
     def _validate_input(self):
         """validate search_term and input_text input
         """
-        if not isinstance(self.search_term, str):
-            self.logger.error('Provide a search term as string: type {} invalid'.format(
+
+        if not isinstance(self.case_sensitive, bool):
+            self.logger.error("case_sensitive is either True or False".format(
                 type(self.search_term)
             ))
             raise ValueError(
-                'Provide a search term as string: type {} invalid'.format(
+                "case_sensitive is either True or False".format(
+                    type(self.search_term)
+                )
+            )
+
+        if not isinstance(self.exact_match, bool):
+            self.logger.error("exact_match is either True or False".format(
+                type(self.search_term)
+            ))
+            raise ValueError(
+                "exact_match is either True or False".format(
+                    type(self.search_term)
+                )
+            )
+        
+
+        if not isinstance(self.search_term, str):
+            self.logger.error("Provide a search term as string: type {} invalid".format(
+                type(self.search_term)
+            ))
+            raise ValueError(
+                "Provide a search term as string: type {} invalid".format(
                     type(self.search_term)
                 )
             )
 
         if not isinstance(self.input_text, str):
-            self.logger.error('Provide an input text as string: type {} invalid'.format(
+            self.logger.error("Provide an input text as string: type {} invalid".format(
                 type(self.input_text)
             ))
             raise ValueError(
-                'Provide an input text as string: type {} invalid'.format(
+                "Provide an input text as string: type {} invalid".format(
                     type(self.input_text)
                 )
             )
 
         if not all([self.search_term, self.input_text]):
             self.logger.error(
-                'Provide a search term and input text as string'
+                "Provide a search term and input text as string"
             )
-            raise ValueError('Provide a search term and input text as string')
+            raise ValueError("Provide a search term and input text as string")
+
+
+        if not self._isin_text():
+            self.logger.error("search_term: {} not in input_text: {}".format(
+                self.search_term,
+                self.input_text
+            ))
+            raise NotFoundError(
+                "search_term: {} not in input_text: {}".format(
+                    self.search_term,
+                    self.input_text
+                )
+            )
 
 
 class NotFoundError(Exception):
     """Custom exception to be raised if search_term is not in input_text
     """
     pass
+    
